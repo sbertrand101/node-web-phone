689cbf7bbe2a1ca08628a056271ac3ed3f2a28e6
Andrey Belchikov <belchikovs@gmail.com>
2016-02-11 13:19:53 +0000

    Fixes in buiid script

--- build-frontend.sh
+++ build-frontend.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# extract submodule if need or download it directly (required for 1-click deploy on Heroku)
-[[ "$(ls -A ./web-sms-chat-frontend)" ]] || (git submodule init && git submodule update) || (wget https://github.com/BandwidthExamples/web-sms-chat-frontend/archive/develop.zip -O web-sms-chat-frontend.zip && unzip -o web-sms-chat-frontend.zip)
+
+if [ -d ./.git ]; then
+  # This is git working copy
+
+  # extract submodule if need
+  [[ "$(ls -A ./web-sms-chat-frontend)" ]] || (git submodule init && git submodule update)
+else
+  # download frontend directly if need (required for 1-click deploy on Heroku)
+  [[ "$(ls -A ./web-sms-chat-frontend)" ]] || (wget https://github.com/BandwidthExamples/web-sms-chat-frontend/archive/develop.zip -O web-sms-chat-frontend.zip && unzip -o web-sms-chat-frontend.zip)
+fi
+
 
 cd ./web-sms-chat-frontend
 npm install
