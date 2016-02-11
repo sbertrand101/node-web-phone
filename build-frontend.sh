ecba39810dd3994c0495c45ebf0104d313b900d9
Andrey Belchikov <belchikovs@gmail.com>
2016-02-11 13:24:39 +0000

    Fixes in buiid script

--- build-frontend.sh
+++ build-frontend.sh
@@ -5,10 +5,10 @@ if [ -d ./.git ]; then
   # This is git working copy
 
   # extract submodule if need
-  [[ "$(ls -A ./web-sms-chat-frontend)" ]] || (git submodule init && git submodule update)
+  [[ "$(ls -A ./web-sms-chat-frontend)" ]] || (git submodule init && git submodule update) || true
 else
   # download frontend directly if need (required for 1-click deploy on Heroku)
-  [[ "$(ls -A ./web-sms-chat-frontend)" ]] || (wget https://github.com/BandwidthExamples/web-sms-chat-frontend/archive/develop.zip -O web-sms-chat-frontend.zip && unzip -o web-sms-chat-frontend.zip)
+  [[ "$(ls -A ./web-sms-chat-frontend)" ]] || (wget https://github.com/BandwidthExamples/web-sms-chat-frontend/archive/develop.zip -O web-sms-chat-frontend.zip && unzip -o web-sms-chat-frontend.zip) || true
 fi
 
 
