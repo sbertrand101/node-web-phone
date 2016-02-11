b52b927ac2137fb108d9ffe4ca8f32a5af60849e
Andrey Belchikov <belchikovs@gmail.com>
2016-02-11 13:12:38 +0000

    Added downloading frontend via wget  if git is missing

--- build-frontend.sh
+++ build-frontend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# extract submodule if need (required for 1-click deploy on Heroku)
-[[ "$(ls -A ./web-sms-chat-frontend)" ]] || (git submodule init && git submodule update)
+# extract submodule if need or download it directly (required for 1-click deploy on Heroku)
+[[ "$(ls -A ./web-sms-chat-frontend)" ]] || (git submodule init && git submodule update) || (wget https://github.com/BandwidthExamples/web-sms-chat-frontend/archive/develop.zip -O web-sms-chat-frontend.zip && unzip -o web-sms-chat-frontend.zip)
 
 cd ./web-sms-chat-frontend
 npm install
