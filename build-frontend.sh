36e6afd5e3b95ac1a64f0762fb7449d606f232fd
Andrey Belchikov <belchikovs@gmail.com>
2016-02-15 08:54:41 +0000

    Fixes in script

--- build-frontend.sh
+++ build-frontend.sh
@@ -10,12 +10,17 @@ else
   # download frontend directly if need (required for 1-click deploy on Heroku)
   [[ "$(ls -A ./web-sms-chat-frontend)" ]] || (wget https://github.com/BandwidthExamples/web-sms-chat-frontend/archive/develop.zip -O web-sms-chat-frontend.zip && unzip -o web-sms-chat-frontend.zip) || true
   # for "master" and "develop" branches
-  [[ -d web-sms-chat-frontend-develop ]] && mv web-sms-chat-frontend-develop web-sms-chat-frontend
-  [[ -d web-sms-chat-frontend-master ]] && mv web-sms-chat-frontend-master web-sms-chat-frontend
+  [[ -d ./web-sms-chat-frontend-develop ]] && mv ./web-sms-chat-frontend-develop ./web-sms-chat-frontend
+  [[ -d ./web-sms-chat-frontend-master ]] && mv ./web-sms-chat-frontend-master ./web-sms-chat-frontend
 fi
 
 ls
 
-cd ./web-sms-chat-frontend
-npm install
-npm run build
+if [ -d ./web-sms-chat-frontend ]; then
+  cd ./web-sms-chat-frontend
+  npm install
+  npm run build
+else
+  echo  "Missing frontend directory!!!"
+  exit 1
+fi
