9c7a341b62be38d7987efabe3efb63e8e6a0c212
Andrey Belchikov <belchikovs@gmail.com>
2016-02-15 08:26:18 +0000

    Fixed an issue in build-frontend

--- build-frontend.sh
+++ build-frontend.sh
@@ -9,6 +9,8 @@ if [ -d ./.git ]; then
 else
   # download frontend directly if need (required for 1-click deploy on Heroku)
   [[ "$(ls -A ./web-sms-chat-frontend)" ]] || (wget https://github.com/BandwidthExamples/web-sms-chat-frontend/archive/develop.zip -O web-sms-chat-frontend.zip && unzip -o web-sms-chat-frontend.zip) || true
+  #mv web-sms-chat-frontend-master web-sms-chat-frontend || true # for "master" and "develop" branches
+  mv web-sms-chat-frontend-develop web-sms-chat-frontend || true
 fi
 
 
