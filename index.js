456dce1a5178c87dd5b6b553595d48472b56ad76
Andrey Belchikov <belchikovs@gmail.com>
2016-02-26 17:23:26 +0300

    Restored work with bundle

--- index.js
+++ index.js
@@ -402,7 +402,7 @@ app.use(function* (next) {
   }
   //SPA support
   if (this.request.method === "GET"
-    && ["/index.html", "/config.js", "/app/", "/styles/", "/node_modules/", "/jspm_packages/", "/sounds/", "/vendor/"].filter(function (t) { return this.request.path.indexOf(t) >= 0; }.bind(this)).length === 0
+    && ["/index.html", "/config.js", "/build.", "/app/", "/styles/", "/node_modules/", "/jspm_packages/", "/sounds/", "/vendor/"].filter(function (t) { return this.request.path.indexOf(t) >= 0; }.bind(this)).length === 0
     && this.request.path !== "/") {
     this.status = 301;
     this.redirect("/");
