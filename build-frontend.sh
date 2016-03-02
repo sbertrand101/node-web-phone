00bf555d34e3d4bab04f5fd4cdf4b3ab24a7a804
Andrey Belchikov <belchikovs@gmail.com>
2016-03-02 12:03:21 +0300

    Updated url to download frontend

--- build-frontend.sh
+++ build-frontend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # download frontend directly if need
-[[  -d ./web-sms-chat-frontend ]] || (wget https://github.com/BandwidthExamples/web-sms-chat-frontend/releases/download/v1.0.1-pre-with-calls/web-sms-chat-frontend.zip -O web-sms-chat-frontend.zip && mkdir web-sms-chat-frontend && unzip -o web-sms-chat-frontend.zip -d ./web-sms-chat-frontend) || true
+[[  -d ./web-sms-chat-frontend ]] || (wget https://github.com/BandwidthExamples/web-sms-chat-frontend/releases/download/v1.0.1-with-calls/web-sms-chat-frontend.zip -O web-sms-chat-frontend.zip && mkdir web-sms-chat-frontend && unzip -o web-sms-chat-frontend.zip -d ./web-sms-chat-frontend) || true
 
